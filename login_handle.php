6928b6d3b27e0ac47d5da8c9e37e74cb7b05c2cf
luissanchez1908 <luissanchez1908@gmail.com>
2019-01-31 12:09:40 -0500

    Update login_handle.php

--- login_handle.php
+++ login_handle.php
@@ -7,12 +7,12 @@ $password= $_POST['Password'];
 //echo $userid;
 //echo $password;
 
-$query = sprintf("SELECT * FROM mediaexpress.users WHERE UserID= '%s' AND Password = '%s'",$_POST['UserID'], $_POST['Password']);
+$query = sprintf("SELECT * FROM database_name WHERE UserID= '%s' AND Password = '%s'",$_POST['UserID'], $_POST['Password']);
 $result = mysqli_query($conn, $query);
 
 	if ($result) {
 		if (mysqli_num_rows($result) > 0) {
-			$query = sprintf("SELECT UserID, Password FROM mediaexpress.users WHERE UPPER(UserID) = '%s' AND Password = '%s'", strtoupper($_POST['UserID']), $_POST['Password']);
+			$query = sprintf("SELECT UserID, Password FROM database_name WHERE UPPER(UserID) = '%s' AND Password = '%s'", strtoupper($_POST['UserID']), $_POST['Password']);
 			$result = mysqli_query($conn, $query);
 			if ($result) {
 				if (mysqli_num_rows($result) > 0) {
